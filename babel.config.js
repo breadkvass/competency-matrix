5bede51e253a0bfd0621b5a6cdbfd29eeb3e82bd
Anastasiya Sulimova <sulimova.a.m@gmail.com>
2025-04-16 14:23:50 +0300

    upd config

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
 module.exports = function (api) {
-  api.cache(true)
+  api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      // Compile Exported Namespaces
-      '@babel/plugin-proposal-export-namespace-from',
-      // React Native Reanimated
-      'react-native-reanimated/plugin',
-      // Use Absolute Imports
-      ['module-resolver', { alias: { src: './src' } }]
-    ]
-  }
-}
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+  };
+};
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,12 @@
 import { Stack } from 'expo-router';
 import React from 'react';
+import "./global.css"
 
 const Layout = () => {
   return (
     <Stack initialRouteName='index'>
       <Stack.Screen name='index' options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)"  options={{ headerShown: false }} />
     </Stack>          
   );
 };
